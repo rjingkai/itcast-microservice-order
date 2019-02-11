3db6c9bd4380d1c45ff21199a9c055e042dcd7cb
renjk <1137497920@qq.com>
2019-02-11 17:51:09 +0800

    新增了eureka集群

--- src/main/java/cn/itcast/microservice/App.java
+++ src/main/java/cn/itcast/microservice/App.java
@@ -4,6 +4,7 @@ package cn.itcast.microservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 public class App 
 {
     @Bean
+  //  @LoadBalanced   //此注解的作用是让rest客户端支持负载均衡
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
--- src/main/java/cn/itcast/microservice/controller/OrderController.java
+++ src/main/java/cn/itcast/microservice/controller/OrderController.java
@@ -1,6 +1,8 @@
 package cn.itcast.microservice.controller;
 
+import cn.itcast.microservice.pojo.Item;
 import cn.itcast.microservice.pojo.Order;
+import cn.itcast.microservice.service.ItemService;
 import cn.itcast.microservice.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +14,18 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    @Autowired
+    private ItemService itemService;
+
 
     @RequestMapping("/order")
     public Order queryOrderById(String id){
         return orderService.queryOrederById(id);
+    }
+
+    @RequestMapping("/item")
+    public Item queryItem(Long id){
+        return itemService.queryItemById(id);
     }
 
 
